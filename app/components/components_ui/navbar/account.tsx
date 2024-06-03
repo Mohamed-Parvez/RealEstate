fedf9bc616aa764fd0986152698a567a90c08a24
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-06-03 22:21:27 +0530

    add responsiveness for account

--- app/components/components_ui/navbar/account.tsx
+++ app/components/components_ui/navbar/account.tsx
@@ -28,13 +28,11 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 const Account = () => {
   const [Notregistered, Registered] = useState(
@@ -42,39 +40,53 @@ const Account = () => {
       <DialogTrigger asChild>
         <Button
           variant="outline"
-          className="small-bold lg:body-bold border-none p-0"
+          className="small-bold lg:body-bold ml-[-24px] border-none"
         >
           REGISTER / LOGIN
         </Button>
       </DialogTrigger>
-      <DialogContent className="bg-white py-10 sm:max-w-screen-xs">
-        <DialogHeader className="gap-6">
-          <DialogTitle className="h3-bold">REGISTER / LOGIN</DialogTitle>
-          <DialogDescription>Please enter your phone number</DialogDescription>
+      <DialogContent className="max-w-[340px] bg-white sm:py-6 md:max-w-screen-xs md:py-10">
+        <DialogHeader className="gap-5">
+          <DialogTitle className="body-bold md:paragraph-bold lg:h3-bold">
+            REGISTER / LOGIN
+          </DialogTitle>
+          <DialogDescription className="extra-small-regular md:small-regular lg:body-regular">
+            Please enter your phone number
+          </DialogDescription>
         </DialogHeader>
 
-        <div className="space-y-14 border-black py-[10px]">
+        <div className="mt-[-8px] space-y-10 border-black sm:py-[6px] md:mt-[-10px] md:space-y-12 md:py-[10px] lg:space-y-14">
           <Input
             id="phone_number"
             placeholder="Phone Number"
-            className="col-span-3 h-12 w-[378px] rounded-[8px] border-slate-300"
+            className="extra-small-regular md:small-regular lg:body-regular col-span-3 w-[290px]
+             rounded-[8px] border-slate-300 text-slate-500 sm:h-10 md:h-12 md:w-[378px]"
           />
           <div className="space-y-2">
             <button
               type="submit"
-              className="rounded-full border border-black bg-black px-[156px] py-3 text-white hover:bg-white hover:text-black"
+              className="extra-small-regular md:small-regular lg:body-regular w-[290px] 
+              rounded-full border border-black bg-black py-[11px] text-white hover:bg-white
+               hover:text-black md:w-[378px] md:py-[13px]"
             >
               Continue
             </button>
-            <p className="text-center text-black">or</p>
-            <div className="flex-center w-[378px] space-x-3 rounded-full border border-black py-3">
+            <p className="extra-small-regular md:small-regular lg:body-regular text-center text-black">
+              or
+            </p>
+            <div
+              className="flex-center w-[290px] space-x-3 rounded-full border border-black py-[6px]
+             md:w-[378px] md:py-3"
+            >
               <Image
                 alt="google"
                 height={24}
                 width={24}
                 src={"/assets/icons/google.svg"}
               />
-              <p>Sign in with Google</p>
+              <p className="extra-small-regular md:small-regular lg:body-regular">
+                Sign in with Google
+              </p>
             </div>
           </div>
         </div>
@@ -105,7 +117,7 @@ const Account = () => {
             </div>
           </div>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-48">
+        <DropdownMenuContent className="w-36 sm:w-40 lg:w-48">
           <DropdownMenuLabel className="mb-1 ml-3">
             {Notregistered}
           </DropdownMenuLabel>
